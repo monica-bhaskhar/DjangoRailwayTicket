98858849858924bff6dc4ab9fa48709b772c407e
monica-bhaskhar <55734096+monica-bhaskhar@users.noreply.github.com>
2020-03-16 20:07:26 +0530

    Update settings.py

--- RailwayTicket/settings.py
+++ RailwayTicket/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway_db',
         'USER': 'postgres',
-        'PASSWORD': 'Bhass$095',
+        'PASSWORD': 'xxx',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
